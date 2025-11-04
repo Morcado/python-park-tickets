9fb48420302378bd16beab97c5e116d8b259be11
Oscar Gonzalez <sefin.oscar.gonzalez@slp.gob.mx>
2025-11-04 15:16:44 -0600

    mostrar tickets diarios y resumen diario

--- read-json.py
+++ read-json.py
@@ -1,35 +1,66 @@
+import glob
 import json
 import os
 import pandas
 
-def print_tree(data, indent=""):
-    if isinstance(data, dict):
-        for key, value in data.items():
-            print(f"{indent}├── {key}")
-            print_tree(value, indent + "│   ")
-    elif isinstance(data, list):
-        for item in data:
-            print_tree(item, indent + "│   ")
+def read_daily_sumary(base_path):
+    file = glob.glob(os.path.join(base_path, r'shiftControl\shiftResults\920\shiftResult*.json'))[0]
+    
+    with open(file, 'r') as f:
+        try:
+            data = json.load(f)
+            df = pandas.json_normalize(data)
+            df = df[['InitialDate', 'FinalDate', 'InitInvoice', 'FinishInvoice', 'Id_PeopleOpening', 'InitialCash', 'InternalControl', 'Diference', 'Id_PeopleClosed', 'FinalCash', 'TotalTaxes', 'TotalwhitTaxes', 'TotalwhitOutTaxes', 'LastInsert']]
+            
+            df = df.T
+            df.to_csv(os.path.join(base_path, "daily_summary.csv"))
+            print(f"DataFrame saved to daily_summary.csv")
+            
+        except FileNotFoundError:
+            print(f"Error: File not found at {base_path}")
+        except json.JSONDecodeError:
+            print("Error: Invalid JSON format")
+
+def read_daily_tickets(base_path):
+    
+    json_files = glob.glob(os.path.join(base_path, r'shiftControl\shiftResults\920\Fer*.json'))
+    dfs = []
+    
+    for file in json_files:
+        with open(file, 'r') as f:
+            try:
+                data = json.load(f)
+                df = pandas.json_normalize(data)
+                df = df[['IdInvoice', 'Id_Transaction', 'InvoiceDate', 'TotalWithoutTaxes', 'TotalTaxes', 'Subtotal', 'Total', 'PaymentDetails.change', 'Reference.Id_TransactionParent']]
+                # df = [
+                #     df['IdInvoice'],
+                #     df['Id_Transaction'], 
+                #     df['InvoiceDate'], 
+                #     df['TotalWithoutTaxes'], 
+                #     df['TotalTaxes'], 
+                #     df['Subtotal'], 
+                #     df['Total'], 
+                #     df['PaymentDetails.change'],
+                #     df['Reference.Id_TransactionParent']
+                # ]
+                #df['InvoiceDate'] = df['InvoiceDate'].astype('datetime64[as]')
+                dfs.append(df)
+
+            except FileNotFoundError:
+                print(f"Error: File not found at {base_path}")
+            except json.JSONDecodeError:
+                print("Error: Invalid JSON format")
+                
+    if dfs: # Check if any dataframes were successfully loaded
+        combined_df = pandas.concat(dfs, ignore_index=True)
+        combined_df.to_csv(os.path.join(base_path, "daily_tickets.csv"), index=False)
+        print(f"DataFrame saved to daily_tickets.csv")
     else:
-        print(f"{indent}└── {data}")
+        combined_df = pandas.DataFrame() # Create an empty DataFrame if no files were rea
+    # print(dfs)    
+    #df.to_csv(os.path.join(base_path, csv_file_name), index=False)
+    #print(f"DataFrame saved to {csv_file_name}")
 
-def read_json_tree(base_path):
-    try:
-        with open(os.path.join(base_path, r"shiftControl\shiftResults\920\shiftResult-920.json"), 'r') as file:
-            data = json.load(file)
-            print(data)
-            # print("JSON Tree Structure:")
-            #print_tree(data)
-        df = pandas.json_normalize(data)
-        csv_file_name = "example_pandas.csv"
-        df.to_csv(os.path.join(base_path, csv_file_name), index=False)
-        print(f"DataFrame saved to {csv_file_name}")
-
-    except FileNotFoundError:
-        print(f"Error: File not found at {base_path}")
-    except json.JSONDecodeError:
-        print("Error: Invalid JSON format")
-
-# Example usage
 base_path = r"C:\Users\oscar.gonzalez\Desktop\python-park-tickets\dataBilling"  # Replace with your JSON file path
-read_json_tree(base_path)
+read_daily_tickets(base_path)
+read_daily_sumary(base_path)
